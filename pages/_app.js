b775f6b7e8fc52d64a73ec09488623b5d0ec382a
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2021-12-11 18:50:37 -0300

    fix: add `chakra ui`

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,11 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ChakraProvider } from '@chakra-ui/react';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-};
-
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
+
+export default MyApp;
