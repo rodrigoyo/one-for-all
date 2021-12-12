cd7cd6caaf92b77d13de88f19d14a3087c820928
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2021-12-12 09:37:48 -0300

    feat: add home page with login

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,96 @@
 import styled from 'styled-components';
+import { useState } from 'react';
+
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  chakra,
+  Box,
+  Link,
+  Avatar,
+  FormControl,
+  FormHelperText,
+  InputRightElement,
+} from '@chakra-ui/react';
+
+import { FaUserAlt, FaLock } from 'react-icons/fa';
+
+const CFaUserAlt = chakra(FaUserAlt);
+const CFaLock = chakra(FaLock);
 
 const Title = styled.h1`
   font-size: 50px;
 `;
 
 export default function Home() {
-  return <Title>My page</Title>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowClick = () => setShowPassword(!showPassword);
+
+  return (
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      height="100vh"
+      // backgroundColor="gray.200"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        flexDir="column"
+        mb="2"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Avatar bg="blue.500" />
+        {/* <Heading color="teal.400">Welcome</Heading> */}
+        <Box minW={{ base: '90%', md: '468px' }}>
+          <form>
+            <Stack
+              spacing={4}
+              p="1rem"
+              // backgroundColor="whiteAlpha.900"
+              boxShadow="md"
+            >
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    color="gray.300"
+                    children={<CFaLock color="gray.300" />}
+                  />
+                  <Input
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Digite a palavra-chave"
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                      {showPassword ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                <FormHelperText textAlign="right">
+                  <Link>Esqueceu?</Link>
+                </FormHelperText>
+              </FormControl>
+              <Button
+                borderRadius={0}
+                type="submit"
+                variant="solid"
+                colorScheme="blue"
+                width="full"
+              >
+                Prosseguir
+              </Button>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 }
