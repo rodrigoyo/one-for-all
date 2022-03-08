14202de7b5ef0c919dd86ee24b055781981ac8c7
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2022-03-08 20:14:27 -0300

    fix: authenticate return

--- pages/api/authenticate.js
+++ pages/api/authenticate.js
@@ -1,9 +1,19 @@
 export default function handler(req, res) {
+  if (!req.body.password) {
+    res.status(400).json({
+      status_code: 0,
+      error_msg: 'Require Params Missing',
+    });
+  }
+
   const { password } = req.body;
 
   if (password !== process.env.MASTER_PASSWORD) {
     res.status(401).json({ message: 'unauthorized' });
   }
 
-  res.status(200).json({ message: 'ok' });
+  res.status(200).json({
+    status_code: 1,
+    message: 'ok',
+  });
 }
