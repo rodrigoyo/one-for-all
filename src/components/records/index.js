0232f03fca1f3967930c33d30af48b211f278062
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2022-03-08 22:17:22 -0300

    feat: add last class on emphasis

--- src/components/records/index.js
+++ src/components/records/index.js
@@ -1,6 +1,16 @@
-import { Flex, Stack, Box, Heading, Spacer, Link } from '@chakra-ui/react';
+import {
+  Flex,
+  Stack,
+  Box,
+  Heading,
+  Spacer,
+  Link,
+  Text,
+} from '@chakra-ui/react';
 
 export default function Records({ list }) {
+  const lastClass = list.length > 5 ? list[list.length - 1] : false;
+
   return (
     <Flex flexDirection="column" justifyContent="center" alignItems="center">
       <Stack
@@ -12,6 +22,36 @@ export default function Records({ list }) {
         alignItems="center"
       >
         <Heading color="blue.400">Gravações das aulas</Heading>
+
+        {lastClass && (
+          <Box
+            bg="red.500"
+            fontSize={14}
+            w="500px"
+            p={2}
+            color="white"
+            key={lastClass.id}
+          >
+            <Box p="2">
+              <Text size="sm">Última aula</Text>
+            </Box>
+            <Spacer />
+            <Flex>
+              <Box p="2">
+                <Heading size="sm">{`${lastClass.id}`}</Heading>
+                <Link href={lastClass.url} isExternal>
+                  <Heading size="sd">{`${lastClass.description}`}</Heading>
+                </Link>
+              </Box>
+              <Spacer />
+              <Box p="2" textAlign="right">
+                <Heading size="sd">Senha</Heading>
+                <Heading size="xs">{`${lastClass.secret}`}</Heading>
+              </Box>
+            </Flex>
+          </Box>
+        )}
+
         {list &&
           list.map((record) => {
             const { id, description, url, secret } = record;
